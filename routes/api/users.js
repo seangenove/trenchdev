c438c56742b03c01dbf091eaba404eeea188adc3
seanyboyswag <2150218@slu.edu.ph>
2020-05-09 18:33:36 +0800

    Added initial logic for user registration api route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult  } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 // @route   GET api/users
 // @desc    Register user
@@ -15,13 +19,48 @@ router.post('/', [
         'Pelase enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
 ],
-(req, res) => {
+async (req, res) => {
     const errors = validationResult(req);
 
     if(!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User Route')
+
+    const {name, email, password} = req.body;
+
+    try {
+        let user = await User.findOne({ email })
+
+        if(user) {
+            return res.status(400).json({ errors: [ { msg: 'User already exists' } ] })
+        }
+
+        console.log('before');
+
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        });
+
+        console.log('after');
+
+        user = new User({name, email, avatar, password});
+
+        // Encrypt password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        // Return jsonwebtoken
+
+        res.send('User registered')
+
+    } catch(err) {
+        console.error(err);
+        res.status(500).send('Server error ');
+    }
 });
 
 module.exports = router;
