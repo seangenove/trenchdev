02f2980830cb46fad6a9ef05ebdb3fd159052958
seanyboyswag <2150218@slu.edu.ph>
2020-05-16 16:02:17 +0800

    Added services (fetchProfileById, fetchAllProfiles, fetchGithubRepositories)

--- client/src/config/Endpoints.js
+++ client/src/config/Endpoints.js
@@ -1,6 +1,8 @@
 const Endpoints = {
     PROFILE: 'api/profile',
+    PROFILE_BY_ID: 'api/profile/user',
     USER_PROFILE: 'api/profile/me',
+    GITHUB_REPOS: 'api/profile/github',
     EXPERIENCE: 'api/profile/experience',
     EDUCATION: 'api/profile/education',
 };
--- client/src/services/ProfileServices.js
+++ client/src/services/ProfileServices.js
@@ -10,6 +10,18 @@ export const fetchProfile = (onSuccess, onFailure) => {
     AppApiRequest(Endpoints.USER_PROFILE, 'GET', onSuccess, onFailure);
 };
 
+export const fetchProfileById = (id, onSuccess, onFailure) => {
+    AppApiRequest(`${Endpoints.PROFILE_BY_ID}/${id}`, 'GET', onSuccess, onFailure);
+};
+
+export const fetchAllProfiles = (onSuccess, onFailure) => {
+    AppApiRequest(Endpoints.PROFILE, 'GET', onSuccess, onFailure);
+};
+
+export const fetchGithubRepositories = (username, onSuccess, onFailure) => {
+    AppApiRequest(`${Endpoints.GITHUB_REPOS}/${username}`, 'GET', onSuccess, onFailure);
+};
+
 export const upsertProfile = (data, onSuccess, onFailure) => {
     AppApiRequest(Endpoints.PROFILE, 'POST', onSuccess, onFailure, data);
 };
